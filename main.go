c0ad0f98bfc7bf780099021f4b6a369a415e0968
shinhagunn <shinhagunn@gmail.com>
2024-02-29 18:09:43 +0700

    up

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host=localhost user=postgres password=123456 dbname=postgres port=6000 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(db)
+}
